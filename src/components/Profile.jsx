925213982e87c256497f89b9d0ce731334045c0c
Bishop <allstarbishop@gmail.com>
2020-03-24 15:32:01 +0300

    version1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="http://greenbelarus.info/files/b87a1631.jpg" alt=""/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+        </div>
+        <div>
+            New Post
+        </div>
+        <div>
+            post 1
+        </div>
+        <div>
+            post 2
+        </div>
+    </div>
+};
+export default Profile;
